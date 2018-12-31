0485ec5e46ea9b51b8de16d17e5128c910f442c6
Your Name <ahutoshsharmacse0032@gmail.com>
2019-01-01 02:08:14 +0530

    first

---
+++ first.py
@@ -0,0 +1,18 @@
+Python 2.2 (#28, Dec 21 2001, 12:21:22) [MSC 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license" for more information.
+IDLE 0.8 -- press F1 for help
+>>> print "hey now vrown cow"
+[DEBUG ON]
+>>> 
+[DEBUG ON]
+>>> 
+[DEBUG ON]
+>>> 
+[DEBUG OFF]
+>>> print "hey now brown cow"
+hey now brown cow
+>>> x=raw_input("Enter name :")
+Enter name :bucky
+>>> print "Hey"+x
+Heybucky
+>>> raw_input("Press<enter>")
